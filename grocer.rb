3324b39f0b02c26d7958033914032dae153e2def
Wesley Chen <weschen1996@gmail.com>
2020-01-18 21:19:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,7 +39,7 @@ def apply_coupons(cart, coupons)
   counter = 0;
   while counter < coupons.length
     cart_items = find_item_by_name_in_collection(coupons[counter][:item], cart)
-    couponed = "#{coupons[counter][:item]}"
+    couponed = "#{coupons[counter][:item]} W/COUPON"
     
   end
   return cart
